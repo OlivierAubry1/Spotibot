7cc7a2c77d2f6f952269ee696f54514629d6a06c
olivieraubry <olivier.aubry@usherbrooke.ca>
2025-11-28 10:45:30 -0500

    updated comments

--- run_forever.sh
+++ run_forever.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-# Print a welcome message
 echo "Starting Spotibot Loop..."
 echo "Press [CTRL+C] to stop everything."
 
-# Start an infinite loop
 while true
 do
-    # Log the restart time so you know when it happened
     echo "---------------------------------"
     echo "Starting bot at $(date)"
     echo "---------------------------------"
     
-    # Run the bot using the Python inside your virtual environment
-    # Note: If your venv folder is named differently, update './venv'
     ./venv/bin/python3 bot.py
-    
-    # The script waits here until bot.py finishes (crashes or is stopped)
-    
-    # Once bot.py stops, print a message and wait
+
     echo "Bot stopped. Restarting in 5 seconds..."
     sleep 5
 done
